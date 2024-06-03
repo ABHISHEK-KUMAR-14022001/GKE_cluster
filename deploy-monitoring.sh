a622efc1ba0bbf1a13ac4956cbfcf29cab5b3cd5
Abhishek kumar <87716036+ABHISHEK-KUMAR-14022001@users.noreply.github.com>
2024-06-03 18:31:57 +0530

    Update deploy-monitoring.sh
    hi

--- deploy-monitoring.sh
+++ deploy-monitoring.sh
@@ -1,39 +1,48 @@
 #!/bin/bash
- 
+
 # Variables
 PROJECT_ID="gcp-monitoring-425305"
 CLUSTER_NAME="cluster-name"
 CLUSTER_ZONE="us-central1"
 NAMESPACE="monitoring"
- 
+
 # Install Helm if not already installed
 if ! command -v helm &> /dev/null; then
   echo "Helm not found, installing..."
   curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 fi
- 
+
 # Configure kubectl to use the GKE cluster
 echo "Configuring kubectl for GKE cluster..."
 gcloud container clusters get-credentials $CLUSTER_NAME --zone $CLUSTER_ZONE --project $PROJECT_ID
- 
+
 # Create the monitoring namespace if it doesn't exist
 echo "Creating namespace $NAMESPACE..."
 kubectl create namespace $NAMESPACE || echo "Namespace $NAMESPACE already exists"
- 
+
 # Add Helm repositories and update
 echo "Adding Helm repositories..."
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
- 
-# Deploy Prometheus
+
+# Deploy Prometheus as a LoadBalancer service
 echo "Deploying Prometheus..."
-helm install prometheus prometheus-community/prometheus --namespace $NAMESPACE
- 
-# Deploy Grafana
+helm install prometheus prometheus-community/prometheus --namespace $NAMESPACE --set server.service.type=LoadBalancer
+
+# Deploy Grafana as a LoadBalancer service
 echo "Deploying Grafana..."
-helm install grafana grafana/grafana --namespace $NAMESPACE --set adminPassword='admin' --set service.type=NodePort
- 
-# Display the services in the monitoring namespace
-echo "Fetching services in the monitoring namespace....."
-kubectl get svc -n $NAMESPACE
+helm install grafana grafana/grafana --namespace $NAMESPACE --set adminPassword='admin' --set service.type=LoadBalancer
+
+# Wait for Prometheus and Grafana services to be ready
+echo "Waiting for Prometheus and Grafana services to be ready..."
+kubectl rollout status deployment prometheus-server -n $NAMESPACE
+kubectl rollout status deployment grafana -n $NAMESPACE
+
+# Get the external IP of Prometheus and Grafana services
+PROMETHEUS_EXTERNAL_IP=$(kubectl get svc prometheus-server -n $NAMESPACE -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+GRAFANA_EXTERNAL_IP=$(kubectl get svc grafana -n $NAMESPACE -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+# Output Prometheus and Grafana URLs
+echo "Prometheus is accessible at: http://$PROMETHEUS_EXTERNAL_IP"
+echo "Grafana is accessible at: http://$GRAFANA_EXTERNAL_IP"
