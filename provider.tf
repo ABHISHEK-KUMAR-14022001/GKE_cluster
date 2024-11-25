9d963ae145de19be2def60297244a4c7d37c6d95
Abhishek kumar <87716036+ABHISHEK-KUMAR-14022001@users.noreply.github.com>
2024-11-25 13:17:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
  # credentials = file("D:/New folder/gcpdevopstest-424606-4e07e34ec952.json")
   project     = "react-idp-app"
-  region      = "us-central1-a"
+  region      = "us-central1"
 }
